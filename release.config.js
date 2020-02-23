adc7997568f1cae6da10663ea50f403c426e4a98
Spencer Elliott <me@elliottsj.com>
2020-02-23 02:12:53 -0500

    chore(*): ensure semantic-release pre-releases from this branch

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 // https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md
 
 module.exports = {
-  branches: ['master'],
+  branches: ['master', { name: 'fix-peer-deps', prerelease: true }],
 };
